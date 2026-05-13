1b876c0bea24fba79d0299a60ba415bfb98bc561
danfc-school <2579083@edu.vaniercollege.qc.ca>
2026-05-13 16:03:58 -0400

    Update Book.java
    Added getters, setters, and toString

--- example/Book.java
+++ example/Book.java
@@ -12,6 +12,39 @@ public class Book extends Item {
     public Book(String itemId, String title) {
         super(itemId, title);
     }
+    
+    //Getters and Setters
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book: " +
+                "ISBN='" + ISBN + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'';
+    }
 
     
 }
