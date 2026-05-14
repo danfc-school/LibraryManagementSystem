870538dc19e6a392614105af58594be5ea04deb7
danfc-school <2579083@edu.vaniercollege.qc.ca>
2026-05-14 14:41:51 -0400

    Update Library.java
    Added add, get, remove and returns items. Also added getter and toString.

--- example/Library.java
+++ example/Library.java
@@ -12,5 +12,41 @@ public class Library {
     public Library(String libraryName) {
         this.libraryName = libraryName;
     }
+    //Adds a new item
+    public void addItem(Item item) {
+        itemCatalogue.put(item.getItemId(), item);
+    }
+
+    //Gets items by its personal id
+    public Item getItemById(String itemId) {
+        Item item = itemCatalogue.get(itemId);
+        return item;
+    }
+
+    //Removes items by their Id's and returns it
+    public Item removeItem(String itemId) {
+        Item removed = itemCatalogue.remove(itemId);
+        return removed;
+    }
+
+    //Returns all the items we got
+    public List<Item> getAllItems() {
+
+        return Collections.unmodifiableList(new ArrayList<>(itemCatalogue.values()));
+    }
+
+    // Getters
+
+    public String getLibraryName() {
+        return libraryName;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Library: " +
+                "libraryName='" + libraryName + '\'' +
+                ", itemCatalogue=" + itemCatalogue;
+    }
     
 }
