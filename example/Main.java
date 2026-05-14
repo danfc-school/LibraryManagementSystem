5a9f39f7e94d0beaf8cb61b7fca84dad912eea29
danfc-school <2579083@edu.vaniercollege.qc.ca>
2026-05-14 15:00:20 -0400

    Update Main.java
    Added 3 books and a basic library system

--- example/Main.java
+++ example/Main.java
@@ -4,5 +4,16 @@ package org.example;
 public class Main {
     static void main() {
 
+        System.out.println("Library Management System - \n");
+
+        Library library = new Library("City Central Library");
+
+        //Books
+        library.addItem(new Book("B-001", "Harry Potter", "123456789", "J.K ROWLING", "MAGIC"));
+        library.addItem(new Book("B-002", "Ubik", "987654321", "Philip K. Dick", "Supernatural"));
+        library.addItem(new Book("B-003", "1984", "135792468", "George Orwell", "Dystopia"));
+
+        System.out.println("\n Descriptions:");
+        library.getAllItems().forEach(item -> System.out.println(" " + item.getDescription()));
     }
 }
